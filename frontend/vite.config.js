62232d57ca652bfc0cefe91781401dec7b1088c5
Vaishalijain10 <vaishalijain.vj.10@gmail.com>
2025-01-31 14:47:58 +0530

    Deployed frontend - generated dist folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", 
+  base: "/",
   build: {
     outDir: "dist",
+    assetsDir: "assets",
+  },
+  server: {
+    port: 5173, // Force Vite to use port 3000
+    open: true, // Auto-opens browser when running dev server
   },
 });
